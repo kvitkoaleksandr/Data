50a551855aeea959d492aa5b11451399db80dd75
Kvitko Aleksandr <kvitok-993@mail.ru>
2025-07-21 09:24:55 +0300

    commit

---
+++ src/main/java/com/yourname/booktracker/Practice.java
@@ -0,0 +1,20 @@
+package com.yourname.booktracker;
+
+public class Practice {
+
+    private int count;
+    private String name;
+
+    public Practice(int count, String name){
+        this.count = count;
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void set(int count) {
+        this.count = count;
+    }
+}
